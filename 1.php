73487d431f8d0e6425444238418e21be014ed280
reachTheEnd <778337607@qq.com>
2018-04-26 22:43:08 +1000

    creat login system

---
+++ 1.php
@@ -0,0 +1,26 @@
+<?php 
+include_once 'header.php';
+?>
+
+ <main role="main" class="container">
+
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <h1>Bootstrap starter template</h1>
+        <h1>Bootstrap starter template</h1>
+        <h1>Bootstrap  starter template</h1>
+        <form action="includes/signup.inc.php" method="post">
+        	<input type="text" name="name" placeholder="name">
+        	<input type="text" name="email" placeholder="E-mail">
+        	<input type="text" name="uid" placeholder="username ">
+        	<input type="password" name="pwd" placeholder="password">
+        	<button type="submit" name="submit">Sign up</button>
+
+        </form>
+      </div>
+
+    </main>
+ 
+<?php 
+include_once 'footer.php';
+?>  
